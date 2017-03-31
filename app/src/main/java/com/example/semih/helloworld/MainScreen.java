99cbc9e49884d9a55f5033de744bdc445f7a5222
Semih <semih.iseri@gmail.com>
2017-03-31 14:19:02 +0300

    travis adjustments

--- app/src/main/java/com/example/semih/helloworld/MainScreen.java
+++ app/src/main/java/com/example/semih/helloworld/MainScreen.java
@@ -1,21 +1,10 @@
 package com.example.semih.helloworld;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-import static com.example.semih.helloworld.R.id.button;
 
 
 public class MainScreen extends AppCompatActivity {
