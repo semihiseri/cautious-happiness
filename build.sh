8d3626e8a6e673c1d3383667a91ba6f5d1fe057f
Semih <semih.iseri@gmail.com>
2017-04-04 16:18:10 +0300

    build script: everything and licenses

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ mkdir /sdk
 cd /sdk
 wget https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
 unzip tools_r25.2.3-linux.zip
-./tools/bin/sdkmanager "platform-tools"
-./tools/bin/sdkmanager "build-tools;25.0.2"
+(while sleep 3; do echo "y"; done) | ./tools/bin/sdkmanager "platform-tools"
+(while sleep 3; do echo "y"; done) | ./tools/bin/sdkmanager "build-tools;25.0.2"
 
 export LC_CTYPE=C.UTF-8
 export ANDROID_HOME=/sdk/
