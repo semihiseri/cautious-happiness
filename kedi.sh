041ab527e43cf039cbd9ebdb9410ea2d512fa44d
Semih <semih.iseri@gmail.com>
2017-04-05 11:47:11 +0300

    jenkins script -kedi- modifications

--- kedi.sh
+++ kedi.sh
@@ -1,1 +1,7 @@
-time /bin/bash -c "KEDI=\$RANDOM ; echo \$KEDI ; sudo docker run --name \$KEDI ubuntu /bin/bash -c \"apt-get update ; apt-get install -y git ; git clone http://github.com/semihiseri/cautious-happiness ; cd cautious-happiness ; chmod 777 build.sh ; ./build.sh\" ; sudo docker cp \$KEDI:/cautious-happiness/app/build/outputs/apk . ; sudo docker rm \$KEDI"
+#!/bin/bash -e
+
+KEDI=$RANDOM 
+echo $KEDI
+sudo docker run --name $KEDI ubuntu /bin/bash -c "apt-get update ; apt-get install -y git ; git clone http://github.com/semihiseri/cautious-happiness ; cd cautious-happiness ; chmod 777 build.sh ; ./build.sh"
+sudo docker cp $KEDI:/cautious-happiness/app/build/outputs/apk . 
+sudo docker rm $KEDI
