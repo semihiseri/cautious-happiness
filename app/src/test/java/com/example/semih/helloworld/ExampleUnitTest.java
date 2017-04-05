675ae295d86c8704691dcc16ef6b7830145d3765
Semih <semih.iseri@gmail.com>
2017-04-05 15:24:45 +0300

    unit tests added

--- app/src/test/java/com/example/semih/helloworld/ExampleUnitTest.java
+++ app/src/test/java/com/example/semih/helloworld/ExampleUnitTest.java
@@ -14,4 +14,20 @@ public class ExampleUnitTest {
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
+
+    @Test
+    public void multiplication_isCorrect() throws Exception {
+        assertEquals(4, 2 * 2);
+    }
+
+    @Test
+    public void subtraction_isCorrect() throws Exception {
+        assertEquals(4, 2 - 2);
+    }
+
+    @Test
+    public void division_isCorrect() throws Exception {
+        assertEquals(4, 2 / 2);
+    }
+
 }
